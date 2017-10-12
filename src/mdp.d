34cdbd4405597d4bb580b427bb141aa4bc9a9267
Kenneth Bogert <kbogert@unca.edu>
2017-10-12 14:42:48 -0400

    Intial commit

---
+++ src/mdp.d
@@ -0,0 +1,44 @@
+
+
+
+class State {
+
+}
+
+class Action {
+
+
+}
+
+class Space {
+
+}
+
+
+class StateAction {
+
+}
+
+class StateActionState {
+
+}
+
+
+// maps from a space to probabilities
+class Distribution {
+
+}
+
+// maps from a space to a Distribution
+class Mapping {
+
+}
+
+class Reward {
+
+
+}
+
+class Model {
+
+}
