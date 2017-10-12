ba4819da38e995a240013b75c3369de4f381f416
Kenneth Bogert <kbogert@unca.edu>
2017-10-12 15:08:39 -0400

    Add discrete mdp skeleton

---
+++ src/discretemdp.d
@@ -0,0 +1,43 @@
+import mdp;
+
+
+class DiscreteSpace : Space {
+
+}
+
+
+
+
+class DiscreteStateSpace : DiscreteSpace {
+
+}
+
+
+class DiscreteActionSpace : DiscreteSpace {
+
+
+}
+
+class DiscreteStateActionSpace : DiscreteSpace {
+
+}
+
+class DiscreteStateActionStateSpace : DiscreteSpace {
+
+}
+
+
+class DiscreteDistribution : Distribution {
+
+}
+
+class DiscreteMapping : Mapping {
+
+
+}
+
+class DiscreteModel : Model {
+
+}
+
+
