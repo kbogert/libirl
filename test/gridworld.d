0bb2677e5997a8964197eb701962ad86ca6d413a
Kenneth Bogert <kbogert@unca.edu>
2017-10-13 13:12:09 -0400

    Begin implementing gridworld
    I don't like this style so much, need a few changes

--- test/gridworld.d
+++ test/gridworld.d
@@ -1,25 +1,95 @@
 import discretemdp;
 
 
+class GridWorldState : discretemdp.DiscreteState {
+     private int x;
+     private int y;
 
-class GridWorldStateSpace : DiscreteStateSpace {
+     public this(int X, int Y) {
+          x = X;
+          y = Y;
+     }
 
-     private State [] states;
+     public int getX() {
+          return x;
+     }
+
+     public int getY() {
+          return y;
+     }
+
+     public override string toString() {
+     }
+
+     override hash_t toHash() const {
+     }
+
+     override bool opEquals(Object o) {
+     }
+
+     override int opCmp(Object o) const {
+     }
+
+}
+
+class GridWorldActionUp : discretemdp.DiscreteAction {
+
+     public GridWorldState intendedNextState(GridWorldState s) {
+           return new GridWorldState(s.getX(), s.getY() - 1);
+
+     }
+}
+
+class GridWorldActionDown : discretemdp.DiscreteAction {
+
+     public GridWorldState intendedNextState(GridWorldState s) {
+           return new GridWorldState(s.getX(), s.getY() + 1);
+
+     }
+
+}
+
+class GridWorldActionLeft : discretemdp.DiscreteAction {
+
+     public GridWorldState intendedNextState(GridWorldState s) {
+           return new GridWorldState(s.getX() - 1, s.getY());
+
+     }
+
+}
+
+class GridWorldActionRight : discretemdp.DiscreteAction {
+
+     public GridWorldState intendedNextState(GridWorldState s) {
+           return new GridWorldState(s.getX() + 1, s.getY());
+
+     }
+
+}
+
+
+
+class GridWorldStateSpace : discretemdp.DiscreteStateSpace {
+
+     private GridWorldState [] states;
 
 
      public State [] getStates() {
           return states;
      }
 
+     public int size() {
+          return states.length;
+     }
 }
 
 
-class GridWorldActionSpace : DiscreteActionSpace {
+class GridWorldActionSpace : discretemdp.DiscreteActionSpace {
 
 }
 
 
-class GridWorldModel : DiscreteModel {
+class GridWorldModel : discretemdp.DiscreteModel {
 
 
      public this(int xSpace, int ySpace, double transitionProb) {
@@ -32,7 +102,7 @@ class GridWorldModel : DiscreteModel {
 
 }
 
-class GridWorldReward : LinearReward {
+class GridWorldReward : discretemdp.LinearReward {
 
 
 }
