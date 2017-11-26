4b2df9f28a25c5d6cf11215619da620a7f2b1ee2
Kenneth Bogert <kbogert@unca.edu>
2017-11-25 19:08:19 -0500

    Function methods need to reduce the parameters
    max(), sumout(), apply() return functions with one less parameter

--- source/mdp.d
+++ source/mdp.d
@@ -10,14 +10,14 @@ class Space {
 class Function (RETURN_TYPE, PARAM ...) {
 
 
-    abstract Function!(RETURN_TYPE, PARAM[0 .. PARAM.length - 1]) max();
+    abstract Function!(RETURN_TYPE, PARAM[0 .. PARAM.length - 2]) max();
 
     abstract PARAM argmax();
 
-    abstract Function!(RETURN_TYPE, PARAM[0 .. PARAM.length - 1]) sumout();
+    abstract Function!(RETURN_TYPE, PARAM[0 .. PARAM.length - 2]) sumout();
 
     // This = (S, A) => R , funct = S => A, returns S => R
-    abstract Function!(RETURNTYPE, B) apply(A : PARAM[PARAM.length - 1], B : PARAM[0 .. PARAM.length - 1] )(Function!(A, B) funct);
+    abstract Function!(RETURN_TYPE, B) apply(A : PARAM[PARAM.length - 1], B : PARAM[0 .. PARAM.length - 2] )(Function!(A, B) funct);
     
 }
 
