aa0c8ec7b5420e9b2bde97da92eac165241ee3ae
Kenneth Bogert <kbogert@unca.edu>
2017-11-29 13:50:48 -0500

    Redifine the Aggregate functions
    Not complete yet, but this should allow for sumout and max to be
    specified over specific parameters, so we could say max(Action)() for
    instance

--- source/mdp.d
+++ source/mdp.d
@@ -1,5 +1,7 @@
 module mdp;
 
+import std.meta;
+
 
 class Space {
 
@@ -10,15 +12,27 @@ class Space {
 class Function (RETURN_TYPE, PARAM ...) {
 
 
-    abstract Function!(RETURN_TYPE, PARAM[0 .. PARAM.length - 2]) max();
+    abstract Function!(RETURN_TYPE, PARAM[0 .. PARAM.length - 2] ) max();
+
+    abstract Function!(RETURN_TYPE, removeLast(ToRemove) ) max(ToRemove ...)();
 
     abstract PARAM argmax();
 
-    abstract Function!(RETURN_TYPE, PARAM[0 .. PARAM.length - 2]) sumout();
+    abstract Function!(RETURN_TYPE, PARAM[0 .. PARAM.length - 2] ) sumout();
+    
+    abstract Function!(RETURN_TYPE, removeLast(ToRemove) ) sumout(ToRemove ...)();
 
     // This = (S, A) => R , funct = S => A, returns S => R
     abstract Function!(RETURN_TYPE, B) apply(A : PARAM[PARAM.length - 1], B : PARAM[0 .. PARAM.length - 2] )(Function!(A, B) funct);
-    
+
+
+    private template removeLast(T) {
+        auto removeLast = Reverse!(Erase!(T, Reverse!(PARAM)));
+    }
+
+    private template removeLast(FIRST, T ...) {
+        auto removeLast = Reverse!(Erase!(FIRST, Reverse!(  removeLast(T) )));
+    }
 }
 
 class Model {
