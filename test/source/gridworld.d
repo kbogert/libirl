8f2181e372f91cf168f365cd3fe39a418cdafa74
Kenneth Bogert <kbogert@unca.edu>
2018-01-17 20:22:03 -0500

    Test for terminal states

--- test/source/gridworld.d
+++ test/source/gridworld.d
@@ -10,10 +10,12 @@ import std.math;
 class GridWorldState : discretemdp.State {
      private int x;
      private int y;
+     private bool terminal;
 
-     public this(int X, int Y) {
+     public this(int X, int Y, bool term = false) {
           x = X;
           y = Y;
+          terminal = term;
      }
 
      public int getX() {
@@ -40,7 +42,7 @@ class GridWorldState : discretemdp.State {
      }
 
      override bool isTerminal() {
-        return false;
+        return terminal;
     }
 }
 
@@ -194,10 +196,6 @@ unittest {
 }
 
 
-@name("Gridworld simulation with terminal state")
-unittest {
-}
-
 @name("Gridworld simulation with non-terminal state")
 unittest {
 
@@ -252,5 +250,91 @@ unittest {
         assert(trajectory[$][0] == optimal_state, "Agent did not reach the optimal state");
     }
     
-   
+}
+
+class GridWorldStateSpaceWithTerminal : discretefunctions.Set!(State) {
+
+
+    public this(int sizeX, int sizeY) {
+        Tuple!(State) [] tempArr;
+        for (int i = 0; i < sizeX; i ++) {
+            for (int j = 0; j < sizeY; j ++) {
+                tempArr ~= tuple(cast(State)new GridWorldState(i, j, ( i == sizeX - 1 && j == sizeY - 1)));
+            }
+        } 
+
+        super(tempArr);
+    }
+
+}
+
+
+@name("Gridworld simulation with terminal state")
+unittest {
+
+    int sizeX = 10;
+    int sizeY = 10;
+    double gamma = 0.95;
+    double value_error = 0.001;
+
+    auto optimal_state = new GridWorldState(sizeX - 1, sizeY - 1);
+    auto optimal_action = new GridWorldAction(1, 0) ;    
+
+    GridWorldStateSpaceWithTerminal states = new GridWorldStateSpaceWithTerminal(sizeX, sizeY);
+    GridWorldActionSpace actions = new GridWorldActionSpace();
+
+    Function!(double [], State, Action) features = new Function!(double [], State, Action)(states.cartesian_product(actions), [0]);
+
+    foreach (a ; actions) {
+        features[ optimal_state , a[0] ] = [1.0];
+    }
+
+    auto lr = new LinearReward(features, [1.0]);
+
+    auto transitions = new ConditionalDistribution!(State, State, Action)(states, states.cartesian_product(actions));
+
+    foreach (s ; states) {
+
+        foreach (a ; actions) {
+
+            auto newState = (cast(GridWorldAction)a[0]).apply(cast(GridWorldState)s[0]);
+
+            Distribution!State ds = new Distribution!(State)(states, 0.0);
+
+            if (s[0] != optimal_state) {
+
+                if (states.contains(cast(State)newState)) {
+                    ds[newState] = 1.0;
+                } else {
+                    ds[s[0]] = 1.0;
+                }
+
+                ds.normalize();
+
+            }    
+            
+            transitions[s[0], a[0]] = ds;
+        }
+    }
+
+    auto model = new BasicModel(states, actions, transitions, lr.toFunction(), gamma, new Distribution!(State)(states, DistInitType.Uniform));
+
+    auto V = value_iteration(model, value_error * max ( max( lr.toFunction())) );
+
+    assert( V[optimal_state] == lr[optimal_state, optimal_action], "Incorrect optimal value with terminal state, " ~ to!string(V[optimal_state]) ~ " correct: " ~ to!string(lr[optimal_state, optimal_action]) );
+
+
+    foreach (i ; 0 .. (sizeX * sizeY) ) {
+        auto trajectory = simulate(model, to_stochastic_policy(optimum_policy(V, model), actions), sizeX + sizeY, model.initialStateDistribution()  );
+        
+        assert(trajectory[$][0] == optimal_state, "Agent did not reach the optimal state");
+
+        if (trajectory.length() > 1) {
+
+            assert(trajectory[$ - 1][0] != optimal_state, "Terminal state is not functioning correctly with simulate");
+
+        }
+        
+    }
+        
 }
