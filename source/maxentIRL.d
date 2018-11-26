5dcb05f677c8498e66bd8616f60b2adcef7d39bd
Kenneth Bogert <kbogert@unca.edu>
2018-11-26 01:24:54 -0500

    Work in progress
    Converting all solvers to work on distributions of trajectories, these
    will eventually be used in the EM approaches

--- source/maxentIRL.d
+++ source/maxentIRL.d
@@ -11,6 +11,7 @@ import std.array;
 import std.algorithm;
 import std.numeric;
 import std.variant;
+import featureexpectations;
 
 /*
 
@@ -25,17 +26,16 @@ import std.variant;
  
 */
 
-// An interface for Max Ent problems that take trajectories as input (sequences of state/action pairs)
-interface Trajectory_IRL_Problem {
-
-    public double [] solve (Sequence!(State, Action)[] trajectories, bool stochasticGradientDescent = true);
+interface Sequence_MaxEnt_Problem(T ...) {
+    
+    public double [] solve (Sequence!(Distribution!(T)) [] trajectories, bool stochasticGradientDescent = true);
 
 }
 
 // Implementation of Ziebart 2008 (Expected Edge Frequency Algorithm)
 // Allows for non-deterministic MDPS, assuming the stochasticity is low and unimportant
 // YMMV
-class MaxEntIRL_Ziebart_approx : Trajectory_IRL_Problem {
+class MaxEntIRL_Ziebart_approx : Sequence_MaxEnt_Problem!(State, Action) {
 
     protected Model model;
     protected double tol;
@@ -57,7 +57,7 @@ class MaxEntIRL_Ziebart_approx : Trajectory_IRL_Problem {
     }
 
 
-    public double [] solve (Sequence!(State, Action)[] trajectories, bool stochasticGradientDescent = true) {
+    public double [] solve (Sequence!(Distribution!(State, Action))[] trajectories, bool stochasticGradientDescent = true) {
 
         double [] returnval = new double[reward.getSize()];
         foreach (i ; 0 .. returnval.length)
@@ -72,8 +72,8 @@ class MaxEntIRL_Ziebart_approx : Trajectory_IRL_Problem {
 
         if (stochasticGradientDescent) {
 
-            auto expert_fe = feature_expectations_per_timestep(trajectories, reward);
-
+            auto expert_fe = feature_expectations_per_timestep(trajectories, &reward.getFeatures, reward.getSize());
+            
 //            sgd_block_size = max(3, max_traj_length / 4);
             sgd_block_size = 1;
             inference_counter = 0;
@@ -83,7 +83,7 @@ class MaxEntIRL_Ziebart_approx : Trajectory_IRL_Problem {
             // normalize initial weights
             returnval[] /= l1norm(returnval);
 
-            auto expert_fe = feature_expectations_from_trajectories(trajectories, reward, max_traj_length);
+            auto expert_fe = feature_expectations_from_trajectories(trajectories, &reward.getFeatures, reward.getSize());
 
             returnval = exponentiatedGradientDescent(expert_fe, returnval.dup, 2.0, tol, size_t.max, max_traj_length, & EEFFeatures);
         }            
@@ -298,7 +298,7 @@ class MaxEntIRL_Ziebart_exact : MaxEntIRL_Ziebart_approx {
 
 // Implementation of Ziebart 2010
 
-class MaxCausalEntIRL_Ziebart : Trajectory_IRL_Problem  {
+class MaxCausalEntIRL_Ziebart : Sequence_MaxEnt_Problem!(State, Action)  {
 
     protected Model model;
     protected double tol;
@@ -320,7 +320,7 @@ class MaxCausalEntIRL_Ziebart : Trajectory_IRL_Problem  {
     }
 
 
-    public double [] solve (Sequence!(State, Action)[] trajectories, bool stochasticGradientDescent = true) {
+    public double [] solve (Sequence!(Distribution!(State, Action)) [] trajectories, bool stochasticGradientDescent = true) {
 
         double [] returnval = new double[reward.getSize()];
         foreach (i ; 0 .. returnval.length)
@@ -335,8 +335,8 @@ class MaxCausalEntIRL_Ziebart : Trajectory_IRL_Problem  {
 
         if (stochasticGradientDescent) {
 
-            auto expert_fe = feature_expectations_per_timestep(trajectories, reward);
-
+            auto expert_fe = feature_expectations_per_timestep(trajectories, &reward.getFeatures, reward.getSize());
+            
 //            sgd_block_size = max(3, max_traj_length / 4);
             sgd_block_size = 1;
             inference_counter = 0;
@@ -346,7 +346,7 @@ class MaxCausalEntIRL_Ziebart : Trajectory_IRL_Problem  {
             // normalize initial weights
             returnval[] /= l1norm(returnval);
 
-            auto expert_fe = feature_expectations_from_trajectories(trajectories, reward, max_traj_length);
+            auto expert_fe = feature_expectations_from_trajectories(trajectories, &reward.getFeatures, reward.getSize());
 
             returnval = exponentiatedGradientDescent(expert_fe, returnval.dup, 2.0, tol, size_t.max, max_traj_length, & Gradient);
         }            
@@ -520,13 +520,13 @@ class MaxCausalEntIRL_SGDApprox : MaxCausalEntIRL_InfMDP {
         super(m, lw, tolerance, true_weights, value_error);
     }
 
-    override public double [] solve (Sequence!(State, Action)[] trajectories, bool stochasticGradientDescent = true) {
+    override public double [] solve (Sequence!(Distribution!(State, Action)) [] trajectories, bool stochasticGradientDescent = true) {
 
         max_traj_length = 0;
         foreach (t ; trajectories)
             if (t.length() > max_traj_length)
-                max_traj_length = t.length();
-                
+                max_traj_length = t.length();                
+
         auto sa_set = model.S().cartesian_product(model.A());
         
         empirical_D_s_a_t = new Distribution!(State, Action) [max_traj_length];
@@ -535,7 +535,8 @@ class MaxCausalEntIRL_SGDApprox : MaxCausalEntIRL_InfMDP {
             
             foreach(traj; trajectories) {
                 if (traj.length > t) {
-                    empirical_D_s_a_t[t][traj[t]] += 1.0;
+                    foreach (sa; traj[t][0].param_set())
+                        empirical_D_s_a_t[t][sa] += traj[t][0][sa];
                 }
             }
             empirical_D_s_a_t[t].normalize();
@@ -617,101 +618,6 @@ class MaxCausalEntIRL_SGDEmpirical : MaxCausalEntIRL_SGDApprox {
 }
 
 
-double [] feature_expectations_from_trajectories(Sequence!(State, Action)[] trajectories, LinearReward reward, size_t normalize_length_to = 0) {
-
-    double [] returnval = new double[reward.getSize()];
-    returnval[] = 0;
-
-    foreach (t; trajectories) {
-        returnval[] += feature_expectations_from_trajectory(t, reward, normalize_length_to)[] / trajectories.length;
-    }
-
-    return returnval;
-}
-
-double [] feature_expectations_from_trajectory(Sequence!(State, Action) trajectory, LinearReward reward, size_t normalize_length_to = 0) {
-
-    double [] returnval = new double[reward.getSize()];
-    returnval[] = 0;
-
-    foreach(sa; trajectory) {
-        if (sa[0].isTerminal() && sa[1] is null) {
-            // the action on the terminal state is null, we're in trouble because the rewards
-            // are defined for state/actions.  We'll just pick any action from the set and
-            // use it, since if the features are defined correctly the action shouldn't
-            // matter for terminal states.
-
-            auto randomAction = reward.toFunction().param_set().getOne()[1];
-            returnval[] += reward.getFeatures(sa[0], randomAction)[];
-        } else {
-            returnval[] += reward.getFeatures(sa[0], sa[1])[];
-        }
-    }
-
-    if (trajectory.length() < normalize_length_to) {
-        if (trajectory[$][0].isTerminal()) {
-            // project the last state outwards to the desired trajectory length
-
-            if (trajectory[$][1] is null) {
-                auto randomAction = reward.toFunction().param_set().getOne()[1];
-                returnval[] += (normalize_length_to - trajectory.length()) * reward.getFeatures(trajectory[$][0], randomAction)[];
-            } else {
-                returnval[] += (normalize_length_to - trajectory.length()) * reward.getFeatures(trajectory[$][0], trajectory[$][1])[];
-            }            
-
-        }
-    }
-    
-    return returnval;
-    
-}
-
-double [][] feature_expectations_per_timestep(Sequence!(State, Action)[] trajectories, LinearReward reward) {
-
-    double [][] returnval;
-
-    while(true) {
-
-        size_t trajectories_found = 0;
-        double [] next_timestep = new double[reward.getSize()];
-        next_timestep[] = 0;
-
-        auto t = returnval.length;
-        foreach(traj ; trajectories) {
-            if (traj.length > t) {
-                trajectories_found ++;
-
-                auto sa = traj[t];
-                if (sa[0].isTerminal() && sa[1] is null) {
-                    auto randomAction = reward.toFunction().param_set().getOne()[1];
-                    next_timestep[] += reward.getFeatures(sa[0], randomAction)[];
-                } else
-                    next_timestep[] += reward.getFeatures(sa[0], sa[1])[];
-            } else {
-                // repeat the last entry in this trajectory to turn this into an infinite terminal
-                auto sa = traj[$];
-                if (sa[0].isTerminal() ) {
-                    if (sa[1] is null) {
-                        auto randomAction = reward.toFunction().param_set().getOne()[1];
-                        next_timestep[] += reward.getFeatures(sa[0], randomAction)[];
-                    } else
-                        next_timestep[] += reward.getFeatures(sa[0], sa[1])[];
-
-                }
-            }
-        }
-        
-        if (trajectories_found == 0)
-            break;
-
-        next_timestep[] /= trajectories.length;
-        returnval ~= next_timestep;
-        
-    }
-
-
-    return returnval;
-}
 
 // TODO: Implement expectation finders; Exact, Gibbs, Matropolis-Hastings, Hybrid MCMC
 // TODO: Implement LME IRL as containing one IRL problem and an expectation solver
