df29a80da623d3c8e83cfac92e06bc0ff00cf1e8
Kenneth Bogert <kbogert@unca.edu>
2019-01-01 20:12:46 -0500

    Run a single test on my laptop
    Rather than run all tests, copy a unit test to run into singletest.d.
    It'll run instead of the rest of the unit tests.

--- test/source/app.d
+++ test/source/app.d
@@ -14,27 +14,36 @@ shared static this()
 {
 	version (unittest) {
 		// disable built-in unit test runner
-		import gridworldmdptest;
-        import gridworldirltest;
-        import discretefunctionstest;
-        import randommdptest;
-        import trajectorytest;
-        import occlusiontest;
-                        
 		import core.runtime;
 		Runtime.moduleUnitTester = () => true;
-//		runUnitTests!(gridworld)(new JsonTestResultWriter("results.json"));
-
-        bool allSuccessful = true;
         
-		allSuccessful &= runUnitTests!(occlusiontest)(new ConsoleTestResultWriter);
-		allSuccessful &= runUnitTests!(trajectorytest)(new ConsoleTestResultWriter);
-		allSuccessful &= runUnitTests!(randommdptest)(new ConsoleTestResultWriter);
-		allSuccessful &= runUnitTests!(gridworldmdptest)(new ConsoleTestResultWriter);
-		allSuccessful &= runUnitTests!(gridworldirltest)(new ConsoleTestResultWriter);
-		allSuccessful &= runUnitTests!(discretefunctionstest)(new ConsoleTestResultWriter);
-
-        assert(allSuccessful, "Unit tests failed.");
         
+
+        version(fullunittest) {
+            import gridworldmdptest;
+            import gridworldirltest;
+            import discretefunctionstest;
+            import randommdptest;
+            import trajectorytest;
+            import occlusiontest;
+                        
+    //		runUnitTests!(gridworld)(new JsonTestResultWriter("results.json"));
+
+            bool allSuccessful = true;
+        
+    		allSuccessful &= runUnitTests!(occlusiontest)(new ConsoleTestResultWriter);
+    		allSuccessful &= runUnitTests!(trajectorytest)(new ConsoleTestResultWriter);
+    		allSuccessful &= runUnitTests!(randommdptest)(new ConsoleTestResultWriter);
+            allSuccessful &= runUnitTests!(gridworldmdptest)(new ConsoleTestResultWriter);
+    		allSuccessful &= runUnitTests!(gridworldirltest)(new ConsoleTestResultWriter);
+    		allSuccessful &= runUnitTests!(discretefunctionstest)(new ConsoleTestResultWriter);
+
+            assert(allSuccessful, "Unit tests failed.");
+        } else {
+            import singletest;
+
+            assert(runUnitTests!(singletest)(new ConsoleTestResultWriter));
+
+        }
 	}
 }
---
+++ test/source/singletest.d
@@ -0,0 +1,17 @@
+module singletest;
+
+import tested;
+import randommdp;
+import discretemdp;
+import discretefunctions;
+import occlusion;
+import maxentIRL;
+import analysis;
+import std.typecons;
+import std.conv;
+import std.random;
+import std.math;
+import gridworld;
+
+import std.stdio;
+
